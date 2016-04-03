7a84139547d50f45c9eb2ba1790f47f7856456ba
dominitz <dominitzroie@gmail.com>
2016-04-03 19:48:18 +0300

    second commit

---
+++ סדנא ב/WSEP/WSEP/bridgeProject/BridgeReal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSEP.forumManagement;
+
+namespace WSEP.bridgeProject
+{
+    class BridgeReal : BridgeProject
+    {
+
+        private IForumSystem system;
+    }
+}
---
+++ סדנא ב/WSEP/WSEPtests/userManagementTests/UserUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WSEP.userManagement;
+
+namespace WSEPtests.userManagementTests
+{
+    [TestClass]
+    public class UserUnitTests
+    {
+        [TestMethod]
+        public void Test_User()
+        {
+            User u = null;
+        }
+    }
+}
