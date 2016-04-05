b49327cbb9ce064165aa46c41b9f856526f88332
quwala <quwala@gmail.com>
2016-04-05 14:16:02 +0300

    Commit for the Exception

---
+++ WSEP/WSEP/forumManagement/InvalidNameException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WSEP.forumManagement
+
+{
+    public class InvalidNameException : Exception
+    {
+        public InvalidNameException(string message) : base(message)
+        {
+        }
+    }
+}
