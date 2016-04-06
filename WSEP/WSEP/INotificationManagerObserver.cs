6281a4a08fdfab4ec2e362a650b6a6323986fcef
Magal <magal@post.bgu.ac.il>
2016-04-06 12:18:21 +0300

    Notification Management added

--- WSEP/WSEP/INotificationManagerObserver.cs
+++ WSEP/WSEP/INotificationManagerObserver.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WSEP.notificationManagement
+namespace WSEP
 {
-    interface INotificationsManager
+    interface INotificationManagerObserver
     {
         // returns true if the email sent successfuly, otherwise false
         bool NotifyPost(string postResponser_username, string postOwner_username, string postOwner_email);
---
+++ WSEP/WSEP/notificationManagementService/NotificationManagerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSEP.notificationManagementService
+{
+    class NotificationManagerService : INotificationManagerObserver
+    {
+        notificationManagement.INotificationsManager notificationManager;
+
+        public NotificationManagerService()
+        {
+            notificationManager = new notificationManagement.NotificationManager();
+        }
+
+        public bool NotifyPost(string postResponser_username, string postOwner_username, string postOwner_email)
+        {
+            return notificationManager.NotifyPost(postResponser_username, postOwner_username, postOwner_email);
+        }
+    }
+}
