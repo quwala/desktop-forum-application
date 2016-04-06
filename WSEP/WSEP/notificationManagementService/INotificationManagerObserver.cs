52c91f6edb380803b797277bf2bfa6a55ba69c47
gal3 <galmor1007@gmail.com>
2016-04-06 16:35:03 +0300

    what are you?

---
+++ WSEP/WSEP/notificationManagementService/INotificationManagerObserver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSEP
+{
+    interface INotificationManagerObserver
+    {
+        // returns true if the email sent successfuly, otherwise false
+        bool NotifyPost(string postResponser_username, string postOwner_username, string postOwner_email);
+    }
+}
