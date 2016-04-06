176b15c7806770b5227615229c150537cb922e9e
gal3 <galmor1007@gmail.com>
2016-04-06 16:36:33 +0300

    added stuff

---
+++ WSEP/WSEP/adapter/IAdapter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSEP.forumManagement.forumHandler;
+
+namespace WSEP.adapter
+{
+    interface IAdapter
+    {
+        bool addForum(string forumName);
+
+        bool containForum(string forumName);
+
+        bool RegisterToForum(string forumName, string userName, string userPassword, string userMail);
+
+        bool ForumLogIn(string forumName, string userName, string userPassword);
+
+        bool SetPolicy(string forumName, int minNumAdmins, int maxNumAdmins, int minNummoderator, int maxNumModerator, string freeText);
+
+        bool addSubForum(string forumName, string subForumName, List<string> moderatorsNames);
+
+        bool addForumAdmin(string forumName, string adminName);
+
+        bool addSubForumModerator(string forumName, string subForumName, string moderatorName);
+
+        bool CreateThread(string forumName, string subForumName, string threadTitle, string content);
+
+        List<Post> getThreadsFromSubForum(string forumName, string subForumName);
+
+
+
+    }
+}
