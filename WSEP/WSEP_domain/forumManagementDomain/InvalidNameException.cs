ad8ca6552b3be937d52dbee846b4e26bdf66eee5
gal3 <galmor1007@gmail.com>
2016-04-09 13:24:56 +0300

    commit

---
+++ WSEP/WSEP_domain/forumManagementDomain/InvalidNameException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WSEP_doamin.forumManagementDomain
+
+{
+    public class InvalidNameException : Exception
+    {
+        public InvalidNameException(string message) : base(message)
+        {
+        }
+    }
+}
